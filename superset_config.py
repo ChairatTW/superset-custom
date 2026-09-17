0adc1f8082999cf53ca074f22c07e165e4adfb71
ChairatTW <144903997+ChairatTW@users.noreply.github.com>
2026-09-17 15:59:14 +0700

    Create superset_config.py

---
+++ superset_config.py
@@ -0,0 +1,51 @@
+import os
+
+
+# =========================================================
+# Secret Key
+# =========================================================
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+# =========================================================
+# PostgreSQL - Superset Metadata Database
+# =========================================================
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "superset-db")
+
+
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql+psycopg2://"
+    f"{POSTGRES_USER}:"
+    f"{POSTGRES_PASSWORD}@"
+    f"{POSTGRES_HOST}:"
+    f"{POSTGRES_PORT}/"
+    f"{POSTGRES_DB}"
+)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# =========================================================
+# Redis
+# =========================================================
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+
+
+CACHE_CONFIG = {
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_",
+    "CACHE_REDIS_HOST": REDIS_HOST,
+    "CACHE_REDIS_PORT": REDIS_PORT,
+    "CACHE_REDIS_DB": 1,
+    "CACHE_REDIS_PASSWORD": REDIS_PASSWORD,
+}
