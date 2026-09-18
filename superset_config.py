57d83c140ddfb203a9a87dcd6c0a5ada94966a54
ChairatTW <144903997+ChairatTW@users.noreply.github.com>
2026-09-18 11:57:48 +0700

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,32 +1,39 @@
 import os
-from sqlalchemy.engine import URL
+from urllib.parse import quote_plus
 
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 
+# =========================================================
+# PostgreSQL Metadata Database
+# =========================================================
+
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
-POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", "5432"))
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "superset-db")
 
 
-SQLALCHEMY_DATABASE_URI = URL.create(
-    drivername="postgresql+psycopg2",
-    username=POSTGRES_USER,
-    password=POSTGRES_PASSWORD,
-    host=POSTGRES_HOST,
-    port=POSTGRES_PORT,
-    database=POSTGRES_DB,
+SQLALCHEMY_DATABASE_URI = (
+    f"postgresql+psycopg2://"
+    f"{quote_plus(POSTGRES_USER)}:"
+    f"{quote_plus(POSTGRES_PASSWORD)}@"
+    f"{POSTGRES_HOST}:"
+    f"{POSTGRES_PORT}/"
+    f"{POSTGRES_DB}"
 )
-
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
+# =========================================================
+# Redis
+# =========================================================
+
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
 
@@ -34,8 +41,9 @@ CACHE_CONFIG = {
     "CACHE_TYPE": "RedisCache",
     "CACHE_DEFAULT_TIMEOUT": 300,
     "CACHE_KEY_PREFIX": "superset_",
+
     "CACHE_REDIS_HOST": REDIS_HOST,
-    "CACHE_REDIS_PORT": REDIS_PORT,
+    "CACHE_REDIS_PORT": int(REDIS_PORT),
     "CACHE_REDIS_DB": 1,
     "CACHE_REDIS_PASSWORD": REDIS_PASSWORD,
 }
